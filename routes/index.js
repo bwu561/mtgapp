1a663bfe28d9b638faf19f7db2d5f02a80751c75
brendan <wu_561@hotmail.com>
2014-12-23 18:36:16 +1100

    routes for lobby and minor changes to lobby

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/login', function(req, res) {
+	res.send('got post');
+});
+
+router.post('/register', function(req, res) {
+	res.send('got post');
+});
+
+router.get('/login', function(req, res) {
+	res.send('got get :P');
+});
+
 module.exports = router;
